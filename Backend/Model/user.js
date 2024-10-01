bc0c236e3fe48bc510903b632b76b347c6abec36
Anushka1205 <anushka.bhatt@kalvium.community>
2024-10-01 12:00:20 +0530

    deployed frontend

--- Backend/Model/user.js
+++ Backend/Model/user.js
@@ -1,10 +1,12 @@
-const mongoose = require('mongoose')
-const userSchema=mongoose.Schema({
-    username: String,
-    password: String,
-    Email: String,
-    phoneno: Number
-})
-const userModel = mongoose.model("usersDB", userSchema)
+const mongoose = require('mongoose');
 
-module.exports={ userModel }
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phoneno: { type: String, required: true }
+});
+
+const userModel = mongoose.model("usersdbs", userSchema);
+
+module.exports = { userModel };
